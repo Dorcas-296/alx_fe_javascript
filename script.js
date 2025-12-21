9e26d18435ba0f89e968fbc5729e7c63f1180e46
Dorcas Okeleji <okelejidorcas@gmail.com>
2025-12-21 21:27:26 +0100

    make changes to pass alx grade

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 const quoteDisplay = document.getElementById("quoteDisplay");
 const button = document.getElementById("newQuote");
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
+const addQuoteBtn = document.getElementById("addQuoteBtn");
 
 const quotes = [
-    { text:"believe in yourself.", category: "motivation" },
-    { text: "know one knows tomorrow.", category: "inspiration" },
-    { text: "You can begin where you are.", category: "motivation" }
+    { text: "Believe in yourself.", category: "Motivation" },
+    { text: "No one knows tomorrow.", category: "Inspiration" },
+    { text: "You can begin where you are.", category: "Motivation" }
 ];
+
 function displayRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const quote = quotes[randomIndex];
     quoteDisplay.innerHTML = `"${quote.text}" <br> <small>Category: ${quote.category}</small>`;
 }
 
-button.addEventListener("click", displayRandomQuote);
-
-const newQuoteText = document.getElementById("newQuoteText");
-const newQuoteCategory = document.getElementById("newQuoteCategory");
-const addQuoteBtn = document.getElementById("addQuoteBtn");
-
 function addQuote() {
     if (newQuoteText.value && newQuoteCategory.value) {
-    const newQuote = {
-        text: newQuoteText.value,
-        category: newQuoteCategory.value
-    };
-
-    quotes.push(newQuote);
-
-    quoteDisplay.innerHTML = `"${newQuote.text}" <br> <small>Category: ${newQuote.caategory}</small>`;
-
-    newQuoteText.value = "";
-    newQuoteCategory.value = "";
-    } else {
-        alert("please fill in both fields!");
+        const newQuote = {
+            text: newQuoteText.value,
+            category: newQuoteCategory.value
+        };
+        quotes.push(newQuote);
+        quoteDisplay.innerHTML = `"${newQuote.text}" <br> <small>Category: ${newQuote.category}</small>`;
+        newQuoteText.value = "";
+        newQuoteCategory.value = "";
     }
 }
+
+button.addEventListener("click", displayRandomQuote);
 addQuoteBtn.addEventListener("click", addQuote);
